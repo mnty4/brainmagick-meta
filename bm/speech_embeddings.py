9e2f868cfb1fde15dfeb91e23c3ad8b8b6617ccc
Lukas Gaspar <zenikyn@gmail.com>
2024-10-25 14:19:04 +1100

    speech embeddings are now average over last 4 hidden states

--- bm/speech_embeddings.py
+++ bm/speech_embeddings.py
@@ -13,7 +13,7 @@ class SpeechEmbeddings:
         self.processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")
         self.model = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-base-960h").to(self.device)
 
-    def get_audio_embeddings(self, wav_path, start, duration):
+    def get_audio_embeddings(self, wav_path, start, duration, average_last_4_hidden_states=True):
         
         audio, rate = librosa.load(os.path.join(self.data_dir, wav_path), sr = 16000, offset=start, duration=duration)
         
@@ -21,7 +21,12 @@ class SpeechEmbeddings:
         input_features = self.processor(audio, sampling_rate=16000, return_tensors="pt").input_values.to(self.device)
         
         with torch.no_grad():
-            outputs = self.model(input_features)
+            outputs = self.model(input_features, output_hidden_states=True)
 
         audio_embeddings = outputs.last_hidden_state
+
+        if average_last_4_hidden_states:
+            hidden_states = outputs.hidden_states[-4:]
+            audio_embeddings = torch.cat(hidden_states).mean(dim=0)
+
         return audio_embeddings
